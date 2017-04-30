5f26d78d32008897f621cfb93046ca6b3e3ec131
Daniel Høyer Iversen <daniel.h.iversen@ntnu.no>
2017-04-30 14:40:48 +0200

    config

--- code/config.py
+++ code/config.py
@@ -1,20 +1,19 @@
 import util
 from keras.optimizers import Adam, RMSprop, SGD
 
-
 net_config = {}
-net_config['batch_size'] = 5
-net_config['nb_epoch'] = 100000
+net_config['batch_size'] = 150
+net_config['nb_epoch'] = 1000000
 net_config['kernel_initializer'] = 'glorot_uniform'
 net_config['padding'] = 'same'
-net_config['activation'] = 'relu'
-net_config['dropout'] = 0
-net_config['dropout2'] = 0
+net_config['activation'] = 'CReLU'
+net_config['dropout'] = 0.4
+net_config['dropout2'] = 0.4
 
-net_config['loss'] = 'binary_crossentropy'
+net_config['loss'] = 'binary_crossentropy' #util.fbeta_loss
 #optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 # optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.3, nesterov=False)
 # optimizer = RMSprop(lr=0.045, rho=0.9, epsilon=1.0)
 #optimizer = "adam" #util.noisyAdam(lr=0.1)
-net_config['optimizer'] = Adam(lr=128)
-net_config['metrics'] = ['accuracy'] #['mean_squared_error', 'mean_absolute_error']
+net_config['optimizer'] = Adam(lr=0.001) # SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True) # Adam(lr=0.5) #  util.Adam_accumulate(accum_iters=12) #
+net_config['metrics'] = [util.fbeta, 'binary_crossentropy'] #['mean_squared_error', 'mean_absolute_error']
